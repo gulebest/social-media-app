d0bd8bce090a0a6122bba488df494baf1dcd32b1
Guluma Tafa <gulumabest694@gmail.com>
2026-01-11 21:44:46 +0300

    Fix typo in type check validation comment

--- src/auth.ts
+++ src/auth.ts
@@ -11,7 +11,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         password: { label: "Password", type: "text" },
       },
       authorize: async (credentials) => {
-        //type check validation
+        //type check vali
         if (
           typeof credentials.email !== "string" ||
           typeof credentials.password !== "string"
